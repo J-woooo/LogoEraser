b2efc098f79f72ad8062c80ce08bea971f34fbd3
wooluck-98 <65936106+wooluck-98@users.noreply.github.com>
2020-09-21 19:25:38 +0900

    .

--- django/upload/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import my_view
-from .views import logo_eraser
-from . import views
-
-urlpatterns = [
-    path('', my_view, name='my-view'),
-    path('result/', logo_eraser, name='result')
-]
